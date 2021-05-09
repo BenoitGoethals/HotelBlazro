577e4fffa57d4bf5272df0c2b04dbd23e86431d0
benoit <benoit.goethals@gmail.com>
2021-05-09 09:36:55 +0200

    dev

--- HotelBlazro/Data/Room.cs
+++ HotelBlazro/Data/Room.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelBlazro.Data
 {
     public class Room
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
         public string Name { get; set; }
         public string RoomNumber { get; set; }
         public int Price { get; set; }
